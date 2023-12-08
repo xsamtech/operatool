63045857385e4ea708e3045f12d1b91d1eef2d23
Xanders <124665689+xsamtech@users.noreply.github.com>
2023-12-08 17:58:29 +0100

    n

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('assets/img/logo-text.png') }}" width="180" alt="">
---
+++ resources/views/components/authentication-card-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="{{ asset('assets/img/logo-text.png') }}" width="250" alt="">
+</a>
